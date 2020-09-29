f1524029b485147a9d912b6c7bf4e665c4c18684
templateK <templateK@github.com>
2020-09-29 17:05:41 +0900

    remove unused imports and it's related codes.

--- mpv_toggle_mute.pyx
+++ mpv_toggle_mute.pyx
@@ -3,13 +3,7 @@ import socket
 import sys
 import os
 import stat
-import json
-import argparse
-import subprocess
-import re
 from enum import Enum
-from AppKit import NSWorkspace
-
 
 
 cdef connect(unix_socket):
@@ -43,33 +37,10 @@ cdef toggle_mute(dest_socket, command):
         # print('closing socket')
         dest_socket.close()
 
-cdef setup_args():
-    parser = argparse.ArgumentParser(description='toggle mpv player sound via json ipc.')
-    if len(sys.argv) == 1:
-        parser.format_help()
-    parser.print_usage = parser.print_help
-    # parser.add_argument('--json', '-j', type=str, required=True, help='focused status of mpv and socket file in json.')
-    # parser.add_argument('--sockets', '-s', nargs='+',
-    #                     type=str, required=False, help='unix socket file path of the mpv player.')
-    # parser.add_argument('--command', '-c', type=str, choices=['toggle', 'mute', 'unmute'], default='toggle')
-    return parser.parse_known_args()
 
 cdef mk_mpvsocket(pid):
     return f'/Volumes/Ramdisk/mpvCache/mpvsocket{pid}'
 
-cdef mpv_status_with_pyobjc():
-    result = []
-    for rapp in NSWorkspace.new().runningApplications():
-        if rapp.bundleIdentifier() == 'io.mpv':
-            socket_file_path = mk_mpvsocket(rapp.processIdentifier())
-            result.append({"path": socket_file_path, "focused": rapp.isActive()})
-    return result
-
-# def log(*msgs):
-#     with open("/Volumes/Ramdisk/tmp/subprocessex.log", 'a+') as f:
-#         for msg in msgs:
-#             f.write(msg)
-#         f.write('\n')
 
 cdef mpv_main(active_socket_path):
     cache_path = "/Volumes/Ramdisk/mpvCache"
@@ -138,4 +109,3 @@ cdef init_server():
 
 if __name__ == '__main__':
     init_server()
-    # mpv_main()
